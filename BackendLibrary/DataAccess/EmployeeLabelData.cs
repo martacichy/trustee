b73f651ecf2776d67a11532a1eb64ab514a763f7
Patryk Murawski <297077@o365.stud.umk.pl>
2021-01-07 21:03:26 +0100

    previous commit again

--- BackendLibrary/DataAccess/EmployeeLabelData.cs
+++ BackendLibrary/DataAccess/EmployeeLabelData.cs
@@ -5,35 +5,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Data;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BackendLibrary.DataAccess
 {
-    /// <summary>
-    ///  Klasa wysyłająca zapytania bazodanowe SQL dotyczące tabeli "EmployeeLabel".
-    /// </summary>
-    class EmployeeLabelData : SqlConnector
     {
-        /// <summary> Dodaje nową etykietę pracownikowi. </summary>
-        public static void AddEmployeeLabel(EmployeeLabelData newEmpLabel)
         {
             using (IDbConnection connection = new MySqlConnection(connectionString))
             {
-                string sql = @"insert into database06.employeelabel (employee_id, label_id)
-                            values (@employee_id, @label_id)";
 
-                connection.Execute(sql, newEmpLabel);
-            }
-        }
-        
-        // Usuwa etykiete pracownikowi
-        public static void DeleteEmployeeLabel(EmployeeLabelData oldEmployeeLabel)
-        {
-            using (IDbConnection connection = new MySqlConnection(connectionString))
-            {
-                string sql = @"delete from database06.employeelabel where employee_id = @employee_id AND label_id = @label_id";
-
-                connection.Execute(sql, oldEmployeeLabel);
             }
         }
 
