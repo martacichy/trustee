04fd014c83c1b4c7949a6710c122b4e49cf4e9bc
Patryk Murawski <297077@o365.stud.umk.pl>
2021-03-15 20:53:05 +0100

    fix update tasks in task-details 0.1

---
+++ BackendLibrary/Validation/EditTaskValidation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BackendLibrary.Validation
+{
+    public class EditTaskValidation
+    {
+        [StringLength(30,
+        ErrorMessage = "Pole 'Tytuł' nie może mieć więcej niż 30 znaków.")]
+        public string name { get; set; }
+
+
+        [StringLength(500,
+        ErrorMessage = "Pole 'Opis' nie może mieć więcej niż 500 znaków.")]
+        public string description { get; set; }
+
+
+        public DateTime start_time { get; set; }
+
+        public DateTime deadline { get; set; }
+
+        [StringLength(50, ErrorMessage = "Pole 'Status' nie może mieć więcej niż 50 znaków.")]
+        public string status { get; set; }
+
+        public int auto_assigned { get; set; }
+    }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class DateDeadlineAttributeEdit : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return (DateTime)value >= DateTime.Today;
+        }
+    }
+
+
+}
