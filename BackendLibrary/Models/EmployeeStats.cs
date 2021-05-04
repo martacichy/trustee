771d75152c532605a2361685c398bc5aaae47cbe
arturkolaska <kolaska128@gmail.com>
2021-05-04 17:34:59 +0200

    add alghoritm output to UI in TaskAdd popup

--- BackendLibrary/Models/EmployeeStats.cs
+++ BackendLibrary/Models/EmployeeStats.cs
@@ -3,6 +3,8 @@
     public class EmployeeStats
     {
         public int Employee_id { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
 
         public double TaskDensity { get; set; }
         public int EmpLabelMatches { get; set; }
@@ -12,7 +14,10 @@
 
         public void CalculateFinalFactor(double weight1, double weight2, double weight3)
         {
-            FinalFactor = TaskDensity * weight1 + EmpLabelMatches * weight2 + TaskLabelMatches * weight3;
+            FinalFactor =
+                1 / (TaskDensity > 0 ? TaskDensity : 0.5) * weight1
+                + EmpLabelMatches * weight2
+                + TaskLabelMatches * weight3;
         }
     }
 }
