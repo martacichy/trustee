e3949c05d8dba8116e05e6dbd240e8943cd134b8
arturkolaska <kolaska128@gmail.com>
2021-05-12 18:57:20 +0200

    add alghoritm for 'Nierozpoczęte' tasks

---
+++ BackendLibrary/Models/TaskStats.cs
@@ -0,0 +1,19 @@
+﻿namespace BackendLibrary.Models
+{
+    public class TaskStats
+    {
+        public int Task_id { get; set; }
+
+        public int EmpLabelMatches { get; set; }
+        public int ArchivalTaskLabelMatches { get; set; }
+
+        public double FinalFactor { get; set; }
+
+        public void CalculateFinalFactor(double weight1, double weight2)
+        {
+            FinalFactor =
+                + EmpLabelMatches * weight1
+                + ArchivalTaskLabelMatches * weight2;
+        }
+    }
+}
